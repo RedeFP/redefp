a6a4c4b5137c4785939f720a5e83aed877e974c1
Alberto B. M. Trevisan <mineplay152@gmail.com>
2019-01-02 11:29:38 -0200

    Reformulação do uso do FTP velho

---
+++ ssftp/load.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Controlador de Arquivos</title>
+    <script src="../vendor/jquery-3.3.1.min.js"></script>
+    <script>
+        $(function(){
+            $("body").load("../ssftp/index.php?protocol=FTP&ftpserver=files.000webhost.com&ftpserverport=21&sshfingerprint=&username=redefp&password_encrypted=659C79005F7D26EF9021E986A59CE0&language=en-utf&skin=shinra&ftpmode=automatic&passivemode=no&viewmode=list&sort=&sortorder=&state=browse&state2=main&directory=%2Fpublic_html&entry=");
+        });
+    </script>
+</head>
+<body>
+    
+</body>
+</html>
