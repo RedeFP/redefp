9157cf59b20b6892030b3385c0a59439edf7312a
juliano <juliano@redefp.000webhostapp.com>
2019-01-05 16:07:50 -0200

    fix head.php

--- res/common/head.php
+++ res/common/head.php
@@ -8,15 +8,6 @@
         <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.css">
         <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap-grid.css">
         <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap-reboot.css">
-<<<<<<< HEAD
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="css/home.css">
-        <link rel="icon" href="css/logo.png">
-	<link rel="shortcut icon" href="/css/logo.png">
-=======
-        <!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css/home.css">
         <link rel="icon" href="./css/logo.png">
->>>>>>> 6c5a0eae472b47947e37c523b6389aff713b93b4
