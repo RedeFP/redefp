1f9a31ea3827081da522067c5147a748534d2489
Alberto B. M. Trevisan <mineplay152@gmail.com>
2019-01-13 15:44:25 -0200

    Merge Conflict Fix - Manual

--- evento.php
+++ evento.php
@@ -8,7 +8,6 @@
         <?php include("./res/common/nav.php"); ?>
         <main role="main" class="container" style="margin-top: 71px; padding-top: 10px;">
 	<div class="jumbotron" style="background-color: #e30613;">
-<<<<<<< HEAD
 		<h1 class="display-4">Nome do Evento</h1>
 		<h3>25/12/2018 23:59</h3>
 		<h4>Quadra da ETEC Fernando Prestes</h6>
@@ -42,93 +41,19 @@
 					</div>
 				</div>
 			</div>
-=======
-		<h1 class="display-4" id="event-name">Nome do Evento</h1>
-		<h3 id="event-date">25/12/2018 23:59</h3>
-		<h4 id="place-name">Quadra da ETEC Fernando Prestes</h6>
-		<h5 id="place-address">Rua Natal, 340 - Jd. Paulistano - Sorocaba</h5>
-		<h6 id="event-owner">Organizado por: Unidade 016</h6>
-	</div>
-	<?php include("./res/common/evento-nav.php"); ?>
-	<div class="jumbotron" style="background-color: #00ff00;">
-		<h2>Ingressos</h2>
-		<h4 id="event-price">R$15,00 + Um quilo de alimento</h4>
-	</div>
-	<div class="jumbotron" style="background-color: #7fffd4;">
-		<h1>Avisos de administradores</h1>
-		<div class="row" id="import1">
->>>>>>> 6c5a0eae472b47947e37c523b6389aff713b93b4
 		</div>
 	</div>
 	<div class="jumbotron" style="background-color: #DA70D6;">
 		<h2>Quem vai?</h2>
-<<<<<<< HEAD
-		<div class="row">
-			<div class="col">
-				<div class="card" style="width:18rem">
-					<img class="card-img-top" src="./photo/IMG201811152232.jpeg" alt="Imagem">
-					<div class="card-body">
-						<h5 class="card-title">Jeovana Kitagaki</h5>
-						<a class="btn btn-primary" href="profile.php?id=18">Ver perfil</a>
-					</div>
-				</div>
-			</div>
-			<div class="col">
-				<div class="card" style="width:18rem">
-					<img class="card-img-top" src="./photo/IMG201810141520.jpg" alt="Imagem">
-					<div class="card-body">
-						<h5 class="card-title">Alberto Benedito de Morais Trevisan</h5>
-						<a class="btn btn-primary" href="profile.php?id=15">Ver perfil</a>
-					</div>
-				</div>
-			</div>
-=======
 		<div class="row" id="import2">
->>>>>>> 6c5a0eae472b47947e37c523b6389aff713b93b4
 		</div>
 	</div>
 	<div class="jumbotron" style="background-color: #FF69B4;">
 		<h2>Quem quer ir?</h2>
-<<<<<<< HEAD
-		<div class="row">
-			<div class="col">
-				<div class="card" style="width:18rem">
-					<img class="card-img-top" src="./photo/IMG201810141943.jpg" alt="Imagem">
-					<div class="card-body">
-						<h5 class="card-title">Laura Silva Dias</h5>
-						<a class="btn btn-primary" href="profile.php?id=16">Ver perfil</a>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-        <!--
-            Informações que deveram ser exibidas (Inicio):
-x            - Nome do Evento
-x            - Horario e Data do Evento
-x            - Nome do Local
-x            - Endereço do Local
-x            - Organizador
-
-x            - Valor do ingresso ou informações sobre entrada
-x            - Quadro de avisos dos administradores
-x            - Pessoas que compareceram (marcados pelo organizador na compra de ingresso) e quem perseguiu
-?            - Denunciar para os administradores de região
-?            - Informar algum problema ao organizador
-
-            Discussão:
-            - Publicações dos Administradores
-            - Publicações dos perseguidores
-            - Galeria
-        -->
-        </main>
-        <?php include("./res/common/modal.php"); ?>
-=======
 		<div class="row" id="import3">
 		</div>
 	</div>
     </main>
     <?php include("./res/common/modal.php"); ?>
->>>>>>> 6c5a0eae472b47947e37c523b6389aff713b93b4
     </body>
 </html>
--- index.php
+++ index.php
@@ -18,14 +18,14 @@
         <div class="container-fluid">
             <div class="jumbotron justify-content-between">
                 <div class="row">
-                    <div class="col-8"><img src="css/logo.png" width="320" height="180"></div>
+                    <div class="col-8"><img src="css/logo.png" style="width:30vw"></div>
                     <div class="col-4" align="right" style="line-height: 180px;padding-right: 30px;">
                         <a href="auth/register.php"><button class="btn btn-danger">Registrar</button></a>
                         <a href="auth/login.php"><button class="btn btn-success">Login</button></a>
                     </div>
                 </div>
             </div>
-            <center><img src="css/logo.png" width="800" height="450"></center>
+            <center><img src="css/logo.png" style="width:800;"></center>
             <p style="text-indent: 2em; font-size: xx-large">A rede social Centrão está para trazer uma inovação à escola técnica de SP, Fernando Prestes uma experiência imerciva e profunda entre colegas de classe e também facilitar a venda de camisetas da escola.</p>
         </div>
     </body>
