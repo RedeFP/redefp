1a3a58c8b85f89076c1d7dace245aa853c2b7f8a
Alberto B. M. Trevisan <mineplay152@gmail.com>
2019-01-13 19:12:55 -0200

    Upload Engine

--- gateway/upload/profile-image.php
+++ gateway/upload/profile-image.php
@@ -1,5 +1,8 @@
 <?php
-$target_dir = "uploads/";
+$aluno = filter_input(INPUT_POST,'aluno');
+$dtpublicacao = filter_input(INPUT_POST,'datetime');
+$txlegenda = filter_input(INPUT_POST,'txlegenda');
+$target_dir = "../../uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
@@ -7,38 +10,42 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
     $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
     if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
+        $error = "File is an image - " . $check["mime"] . ".";
         $uploadOk = 1;
     } else {
-        echo "File is not an image.";
+        $error = "File is not an image.";
         $uploadOk = 0;
     }
 }
 // Check if file already exists
 if (file_exists($target_file)) {
-    echo "Sorry, file already exists.";
+    $error = "Sorry, file already exists.";
     $uploadOk = 0;
 }
 // Check file size
-if ($_FILES["fileToUpload"]["size"] > 500000) {
-    echo "Sorry, your file is too large.";
+if ($_FILES["fileToUpload"]["size"] > 5000000) {
+    $error = "Sorry, your file is too large.";
     $uploadOk = 0;
 }
 // Allow certain file formats
 if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 && $imageFileType != "gif" ) {
-    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $error = "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
     $uploadOk = 0;
 }
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
-    echo "Sorry, your file was not uploaded.";
+    echo $error;
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+        include("../../res/bd.php");
+        $query = mysqli_query($bd,"INSERT INTO aluno_galeria (id_aluno,dtpublicacao,txlegenda,image_url) VALUES ('$aluno','$dtpublicacao','$txlegenda','$target_file')");
+        error_reporting(E_ALL);
+        if(mysqli_error($bd) != "") { echo mysqli_error($bd); }
+        header("Location: ../../profile-pictures.php");
     } else {
-        echo "Sorry, there was an error uploading your file.";
+        echo $error;
     }
 }
 ?>
