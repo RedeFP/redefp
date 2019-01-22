36f43e8edbfaf887011e6e57bb68e0ea14af3c43
Alberto B. M. Trevisan <mineplay152@gmail.com>
2019-01-22 17:29:17 -0200

    Fix Login Page, soon CSS

--- auth/login.php
+++ auth/login.php
@@ -2,20 +2,25 @@
 <html>
 <head>
     <meta charset="UTF-8">
-        <title>RedeFP - Uma rede social escolar</title>
-        <link rel="stylesheet" href="../css/index.css">
-        <link rel="icon" href="css/logo.png">
-        <script src="../js/login.js"></script>
-        <script src="../../vendor/jquery-3.3.1.min.js"></script>
-        <script src="../../vendor/popper.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
+    <title>RedeFP - Uma rede social escolar</title>
+    <link rel="stylesheet" href="../css/index.css">
+    <link rel="icon" href="css/logo.png">
+    <script src="../js/login.js"></script>
+    <script src="../../vendor/jquery-3.3.1.min.js"></script>
+    <script src="../../vendor/popper.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.js"></script>
+    <script src="../vendor/bootstrap/js/bootstrap.bundle.js"></script>
+    <script src="../js/libGET.js"></script>
+    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap-grid.css">
+    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap-reboot.css">
+    <script src="../vendor/mobile-detect.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body style="background: lightgrey">
-    <div class="container-fluid">
+    <div class="container-fluid" style="padding-left: 0px;padding-right: 0px;">
         <div class="jumbotron" style="padding: 5px;">
-            <a href="../"><img src="../css/logo.png" width="320" height="180"></a>
+            <a href="../"><img src="../css/logo.png" style="width:50%; display: block; margin-left: auto; margin-right: auto;"></a>
         </div>
         <div id="error"></div>
         <div class="container" style="background: #ffffff;padding-top: 10px;padding-bottom: 10px;border-radius: 5px;width: 50vh;">
@@ -31,7 +36,7 @@
                     <input type="password" class="form-control" id="inputPassword" placeholder="*****">
                 </div>
             </div>
-            <div class="col-sm-12" align="right">
+            <div class="col" style="text-align: center">
                 <input type="submit" onclick="Submit()" class="btn btn-success" value="Entrar"/>
             </div>
         </div>
