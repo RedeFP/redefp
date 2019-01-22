6d6b3de25dfbacb00de195957c14223f8065e83a
Alberto B. M. Trevisan <mineplay152@gmail.com>
2019-01-22 17:06:40 -0200

    Fix Index Mobile

--- index.php
+++ index.php
@@ -13,20 +13,22 @@
 <html>
     <head>
         <?php include('./res/common/head.php'); ?>
+        <script src="js/index.js"></script>
     </head>
     <body>
+        <link rel="stylesheet" href="css/index.css">
         <div class="container-fluid">
-            <div class="jumbotron justify-content-between">
+            <div class="jumbotron justify-content-between mob">
                 <div class="row">
-                    <div class="col-8"><img src="css/logo.png" style="width:30vw"></div>
-                    <div class="col-4" align="right" style="line-height: 180px;padding-right: 30px;">
+                    <div class="col mob2"><img src="css/logo.png" style="width:30vw"></div>
+                    <div class="col btnicon">
                         <a href="auth/register.php"><button class="btn btn-danger">Registrar</button></a>
                         <a href="auth/login.php"><button class="btn btn-success">Login</button></a>
                     </div>
                 </div>
             </div>
-            <center><img src="css/logo.png" style="width:800;"></center>
-            <p style="text-indent: 2em; font-size: xx-large">A rede social Centrão está para trazer uma inovação à escola técnica de SP, Fernando Prestes uma experiência imerciva e profunda entre colegas de classe e também facilitar a venda de camisetas da escola.</p>
+            <img src="css/logo.png" class="indexlogo">
+            <p style="font-size: medium; text-align: justify">A rede social Centrão está para trazer uma inovação à escola técnica de SP, Fernando Prestes uma experiência imerciva e profunda entre colegas de classe e também facilitar a venda de camisetas da escola.</p>
         </div>
     </body>
 </html>
---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(function(){
+    var md = new MobileDetect(window.navigator.userAgent);
+    if(md.mobile() != "null") {
+        $("button").first().remove();
+        $(".btnicon").css("padding-right","10%")
+    }
+})
