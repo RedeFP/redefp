ea180d774bb9b6409e328b2f02a0e845b201bc3a
Alberto Trevisan <alberto.trevisan@etec.sp.gov.br>
2019-07-24 13:49:04 -0300

    Create notes.php

---
+++ prp/notes.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include("../res/common/prphead.php"); ?>
+    </head>
+    <body>
+        <?php include("../res/common/prp-nav.php"); ?>
+        <main role="main" class="container" style="margin-top: 71px; padding-top: 10px;">
+        </main>
+        <?php include("../res/common/modal.php"); ?>
+    </body>
+</html>
