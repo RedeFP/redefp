0e929f6b756b40dc26273bb7e8f6297a98df20ea
Alberto Trevisan <alberto.trevisan@etec.sp.gov.br>
2019-08-01 19:26:44 -0300

    0.4.5 Notes UX

--- prp/notes.php
+++ prp/notes.php
@@ -6,15 +6,83 @@
     <body>
         <?php include("../res/common/prp-nav.php"); ?>
         <main role="main" class="container" style="margin-top: 71px; padding-top: 10px;">
-            <?php 
-                $id = $_COOKIE[''];
-                mysqli_query($bd,"SELECT * FROM notes WHERE idprofessores = '$id'");
+            <div class="row">
+                <div class="col-3">
+                    <!-- List group -->
+                    <div class="list-group" id="myList" role="tablist">
+                        <a class="list-group-item list-group-item-action active" data-toggle="list" href="#home" role="tab">
+                            <h5>Admirável Chip Novo</h5>
+                            <div class="descriptions">Pane no sistema, alguém me desconfigurou</div>
+                        </a>
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#profile" role="tab">Profile</a>
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#messages" role="tab">Messages</a>
+                        <a class="list-group-item list-group-item-action" data-toggle="list" href="#settings" role="tab">Settings</a>
+                    </div>
+                </div>
+                <div class="col-6">
+                    <!-- Tab panes -->
+                    <div class="tab-content">
+                        <div class="tab-pane active" id="home" role="tabpanel">
+                            <h5>Admirável Chip Novo</h5>
+                            <p class="notes">Pane no sistema, alguém me desconfigurou<br>
+                                Aonde estão meus olhos de robô?<br>
+                                Eu não sabia, eu não tinha percebido<br>
+                                Eu sempre achei que era vivo<br>
+                                Parafuso e fluído em lugar de articulação<br>
+                                Até achava que aqui batia um coração<br>
+                                Nada é orgânico, é tudo programado<br>
+                                E eu achando que tinha me libertado<br>
+                                Mas lá vem eles novamente e eu sei o que vão fazer<br>
+                                Reinstalar o sistema<br>
+                                <br>
+                                Pense, fale, compre, beba<br>
+                                Leia, vote, não se esqueça<br>
+                                Use, seja, ouça, diga<br>
+                                Tenha, more, gaste e viva<br>
+                                <br>
+                                Pense, fale, compre, beba<br>
+                                Leia, vote, não se esqueça<br>
+                                Use, seja, ouça, diga<br>
+                                <br>
+                                Não senhor, sim senhor, não senhor, sim senhor<br>
+                                <br>
+                                Pane no sistema, alguém me desconfigurou<br>
+                                Aonde estão meus olhos de robô?<br>
+                                Eu não sabia, eu não tinha percebido<br>
+                                Eu sempre achei que era vivo<br>
+                                Parafuso e fluido em lugar de articulação<br>
+                                Até achava que aqui batia um coração<br>
+                                Nada é orgânico, é tudo programado<br>
+                                E eu achando que tinha me libertado<br>
+                                Mas lá vem eles novamente e eu sei o que vão fazer<br>
+                                Reinstalar o sistema<br>
+                                <br>
+                                Pense, fale, compre, beba<br>
+                                Leia, vote, não se esqueça<br>
+                                Use, seja, ouça, diga<br>
+                                Tenha, more, gaste e viva<br>
+                                <br>
+                                Pense, fale, compre, beba<br>
+                                Leia, vote, não se esqueça<br>
+                                Use, seja, ouça, diga<br>
+                                <br>
+                                Não senhor, sim senhor, não senhor, sim senhor<br>
+                                <br>
+                                Mas lá vem eles novamente e eu sei o que vão fazer<br>
+                                Reinstalar o sistema<br>
+                            </p>
+                        </div>
+                        <div class="tab-pane" id="profile" role="tabpanel">2</div>
+                        <div class="tab-pane" id="messages" role="tabpanel">3</div>
+                        <div class="tab-pane" id="settings" role="tabpanel">4</div>
+                    </div>
+                </div>
 
+                
 
+                
 
-
-
-            ?>
+            </div>
         </main>
         <?php include("../res/common/modal.php"); ?>
     </body>
