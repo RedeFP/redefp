d81350e9d8e7f44697bf073360a6ca0aaf16210f
Alberto <master@MacBook-Pro-de-Alberto.local>
2019-10-25 00:34:20 -0300

    merge

--- js/libGET.js
+++ js/libGET.js
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-
-var $_GET = {};
-if(document.location.toString().indexOf('?') !== -1) {
-    var query = document.location
-                   .toString()
-                   // get the query string
-                   .replace(/^.*?\?/, '')
-                   // and remove any existing hash string (thanks, @vrijdenker)
-                   .replace(/#.*$/, '')
-                   .split('&');
-
-    for(var i=0, l=query.length; i<l; i++) {
-       var aux = decodeURIComponent(query[i]).split('=');
-       $_GET[aux[0]] = aux[1];
-    }
-}
-=======
 
 var $_GET = {};
 if(document.location.toString().indexOf('?') !== -1) {
@@ -44,4 +26,3 @@ function hideModal() {
 function parseUser() {
     return JSON.parse(localStorage.user);
 }
->>>>>>> 9efad11b96089a109b98b8fd30234bb035a5d9b9
--- js/profile_contact.js
+++ js/profile_contact.js
@@ -1,54 +1,3 @@
-<<<<<<< HEAD
-function loadPerfil()
-{
-    var id;
-    if(typeof($_GET['id']) != "undefined") {
-        id = $_GET['id'];    
-    } else {
-        id = JSON.parse(localStorage.user)['id'];
-    }
-    $.get("gateway/getJSON.php",{f:"one-profile", id: id}, function(result){
-        aluno = JSON.parse(result);
-        if(aluno.profile_pic_url != "") {
-            $(".user-pic").attr("src",aluno.profile_pic_url);
-        }
-        $("#aluno_id").attr("href","profile.php?id="+aluno.id);
-        $("#aluno_id").html(aluno.nome);
-        $("#aluno_apelido").html("@"+aluno.apelido);
-        document.getElementById("aluno_apelido").setAttribute("href","profile.php?id="+aluno.id);
-        $("#l1").attr("href","profile.php?id="+aluno.id);
-        $("#l2").attr("href","profile-pictures.php?id="+aluno.id);
-        $("#l3").attr("href","profile-comunity.php?id="+aluno.id);
-        $("#l4").attr("href","profile-contact.php?id="+aluno.id);
-        $("#mail").html(aluno.institucional);
-        $("#mail").attr("href","malito:"+aluno.institucional);
-        var str = aluno.telefone;
-        var res = str.slice(0, 2);
-        var res2 = str.slice(2, 7);
-        var res3 = str.slice(7,11);
-        var tel = "("+res+")"+res2+"-"+res3;
-        $("#tel").html(tel);
-        $("#tel").attr("href","tel:"+aluno.telefone);
-    });
-}
-
-$(function(){
-    md = new MobileDetect(window.navigator.userAgent);
-    loadPerfil();
-    $(".nav-item")[1].setAttribute("class","nav-item active");
-    $(document).one("ajaxStop",function(){
-        if(md.mobile() != null){
-            $(".col-3").remove();
-            $(".col-9").attr("class","col");
-            if(typeof($_GET['id']) != "undefined") {
-                id = $_GET['id'];    
-            } else {
-                id = JSON.parse(localStorage.user)['id'];
-            }
-            loadNavMob(id);
-        }
-    })
-=======
 function loadPerfil()
 {
     var id;
@@ -175,5 +124,4 @@ $(function(){
         }
     });
     $(".user-pic").click(modalFotoPerfil);
->>>>>>> 9efad11b96089a109b98b8fd30234bb035a5d9b9
 });
